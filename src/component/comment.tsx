2dabf1fb494a59303ef8f7e9f7867bb3c9d0982d
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-22 11:23:44 +0200

    articles and comments

---
+++ src/component/comment.tsx
@@ -0,0 +1,56 @@
+import React, { Fragment } from "react";
+
+export interface User {
+  username: string;
+}
+
+export interface CommentProps {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  body: string;
+  author: User;
+}
+
+const Commnets: React.FC<CommentProps> = ({
+  createdAt,
+  updatedAt,
+  body,
+  id,
+  author,
+}) => {
+  return (
+    <Fragment>
+      <div className="article-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-xs-12 col-md-8 offset-md-2">
+              <div key={id} className="card">
+                <div className="card-block">
+                  <p className="card-text">{body}</p>
+                </div>
+                <div className="card-footer">
+                  <a href="" className="comment-author">
+                    <img
+                      src="http://i.imgur.com/Qr71crq.jpg"
+                      className="comment-author-img"
+                    />
+                  </a>
+                  &nbsp;
+                  <a href="" className="comment-author">
+                    {author.username}{" "}
+                  </a>
+                  <span className="date-posted">
+                    {updatedAt.toDateString()}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Commnets;
