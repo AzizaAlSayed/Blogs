011a9bd46624fe0b76c22455dca76a4c36ea3fc7
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-22 21:35:52 +0200

    post a article

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React, { FC, ReactElement, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import axios from "axios";
 import NewArticle from "./components/newArticle";
 
 const App: React.FC = () => {
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/articles" element={<NewArticle />} />
         </Routes>
       </BrowserRouter>
-      <NewArticle></NewArticle>
     </div>
   );
 };
--- src/components/newArticle.tsx
+++ src/components/newArticle.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import axios from "axios";
 
 interface Tag {
   tage: string;
@@ -12,45 +14,69 @@ interface NewArticleProps {
 }
 
 const NewArticle: React.FC = ({}) => {
+  const [newArticle, setNewArticle] = useState<NewArticleProps>();
+  const { register, handleSubmit, reset } = useForm<NewArticleProps>({});
+
+  const onSubmit = (data: NewArticleProps) => {
+    axios
+      .post("https://api.realworld.io/api/articles", newArticle)
+      .then((response) => {
+        setNewArticle(response.data.newArticle);
+        console.log(JSON.stringify(data, null, 2));
+      });
+  };
+
   return (
     <div className="editor-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-10 offset-md-1 col-xs-12">
-            <form method="post">
+            <form>
               <fieldset>
                 <fieldset className="form-group">
                   <input
+                    {...register("title")}
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article Title"
+                    name="title"
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <input
+                    {...register("description")}
                     type="text"
                     className="form-control"
                     placeholder="What's this article about?"
+                    name="description"
                   />
                 </fieldset>
                 <fieldset className="form-group">
                   <textarea
+                    {...register("body")}
                     className="form-control"
                     rows={8}
                     placeholder="Write your article (in markdown)"
+                    name="body"
                   ></textarea>
                 </fieldset>
                 <fieldset className="form-group">
                   <input
+                    {...register("tagList")}
                     type="text"
                     className="form-control"
                     placeholder="Enter tags"
+                    name="tagList"
                   />
                   <div className="tag-list"></div>
                 </fieldset>
                 <button
                   className="btn btn-lg pull-xs-right btn-primary"
-                  type="submit"
+                  type="button"
+                  onClick={() => {
+                    handleSubmit(onSubmit);
+                    reset();
+                  }}
                 >
                   Publish Article
                 </button>
