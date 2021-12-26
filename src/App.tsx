aef141de257c5518794a15924dd685e4d1b7f2e7
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-26 14:44:29 +0200

    static login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/HomePage/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ArticleProps } from "./components/HomePage/article";
 import axios from "axios";
+import Login from "./components/Login/login";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/articles" element={<Home />} />
+          <Route path="/users/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
