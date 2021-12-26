05fa15dfa3b2fa83b6ed98871e770505566804a6
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-26 10:42:51 +0200

    static profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/HomePage/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ArticleProps } from "./components/HomePage/article";
 import axios from "axios";
+import Profile from "./components/ProfilePage/profile";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/articles" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/ProfilePage/profile.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import Article, { ArticleProps } from "../HomePage/article";
+import axios from "axios";
+
+const Profile: React.FC<{}> = () => {
+  return (
+    <div className="ng-scope">
+      <div className="profile-page ng-scope">
+        <div className="user-info">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-md-10 offset-md-1">
+                <img
+                  ng-src="https://api.realworld.io/images/demo-avatar.png"
+                  className="user-img"
+                  src="https://api.realworld.io/images/demo-avatar.png"
+                />
+                <h4 ng-bind="::$ctrl.profile.username" className="ng-binding">
+                  Gerome
+                </h4>
+                <button
+                  className="btn btn-sm action-btn ng-binding btn-outline-secondary"
+                  ng-class="{ 'disabled': $ctrl.isSubmitting,
+              'btn-outline-secondary': !$ctrl.user.following,
+              'btn-secondary': $ctrl.user.following }"
+                  ng-click="$ctrl.submit()"
+                >
+                  <i className="ion-plus-round"></i>
+                  &nbsp; Follow Gerome
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="feed-toggle">
+                <ul className="nav nav-pills outline-active">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="">
+                      My Articles
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link " href="">
+                      Favorited Articles
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
