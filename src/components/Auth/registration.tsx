ab1aecd64825e6c69f2974e9664a4ab01ae9beee
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-29 15:33:21 +0200

    register

--- src/components/Auth/registration.tsx
+++ src/components/Auth/registration.tsx
@@ -1,11 +1,25 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { setDefaultResultOrder } from "dns";
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 interface Credentials {
   email: string;
   username: string;
   password: string;
+}
+
+interface User {
+  email: string;
+  token: string;
+  username: string;
+  bio: string;
+  image: string;
+  password: string;
+
+}
+
+interface CredentialsProps {
+  credential: User;
 }
 
 const initialCredentials = {
@@ -15,32 +29,54 @@ const initialCredentials = {
 };
 
 const SignUp: React.FC = () => {
-  const [error, setError] = useState("");
   const [credentials, setCredentials] =
     useState<Credentials>(initialCredentials);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Credentials>({});
+
+  const onSiginUp = (data: Credentials) => {
+    axios
+      .post<CredentialsProps>("https://api.realworld.io/api/users", data)
+      .then((response) => {
+        console.log(JSON.stringify(response.data, null, 2));
+        setCredentials(response.data.credential);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data + error.response.status);
+        } else {
+          console.log("Error", error.message);
+        }
+      });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3) col-xs-12">
             <h1 className="text-xs-center">Sign up</h1>
-            <form>
+            <form onSubmit={handleSubmit(onSiginUp)}>
               <fieldset className="form-group">
-                <label htmlFor="username"></label>
                 <input
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="username"
                   name="username"
+                  {...register("username")}
                 />
               </fieldset>
               <fieldset className="form-group">
-                <label htmlFor="email"></label>
                 <input
                   className="form-control form-control-lg"
                   type="email"
                   placeholder="email"
                   name="email"
+                  {...register("email")}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -50,8 +86,12 @@ const SignUp: React.FC = () => {
                   type="password"
                   placeholder="Password"
                   name="password"
+                  {...register("password")}
                 />
               </fieldset>
+              {errors.password && errors.email && errors.username && (
+                <p>This field is required</p>
+              )}
               <button
                 className="btn btn-lg btn-primary pull-xs-right"
                 type="submit"
