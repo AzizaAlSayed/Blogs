7178d0cea2f4fd44e94ec7684ad8e0fe8ef45da2
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2021-12-29 22:04:52 +0200

    add articles

--- src/components/ProfilePage/profile.tsx
+++ src/components/ProfilePage/profile.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Article, { ArticleProps } from "../HomePage/article";
+import { ArticlesResponse } from "../HomePage/home";
 
 const Profile: React.FC<{}> = () => {
   const params = useParams();
-  const url = `https://api.realworld.io/api/profiles/${params.username}`;
+  const [articles, setArticles] = useState<ArticleProps[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<ArticlesResponse>(
+        `https://api.realworld.io/api/articles?author=${params.username}`
+      )
+      .then((response) => {
+        setArticles(response.data.articles);
+      });
+  }, []);
+
   return (
     <div className="ng-scope">
       <div className="profile-page ng-scope">
@@ -32,10 +47,11 @@ const Profile: React.FC<{}> = () => {
             </div>
           </div>
         </div>
-        <div className="container page">
+
+        <div className="container">
           <div className="row">
-            <div className="col-md-9">
-              <div className="feed-toggle">
+            <div className="col-xs-12 col-md-10 offset-md-1">
+              <div className="articles-toggle">
                 <ul className="nav nav-pills outline-active">
                   <li className="nav-item">
                     <a className="nav-link active" href="">
@@ -43,12 +59,24 @@ const Profile: React.FC<{}> = () => {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link " href="">
+                    <a className="nav-link" href="">
                       Favorited Articles
                     </a>
                   </li>
                 </ul>
               </div>
+              {articles.map((article) => (
+                <Article
+                  key={article.id}
+                  id={article.id}
+                  body={article.body}
+                  description={article.description}
+                  tagList={article.tagList}
+                  updatedAt={article.updatedAt}
+                  title={article.title}
+                  author={article.author}
+                ></Article>
+              ))}
             </div>
           </div>
         </div>
