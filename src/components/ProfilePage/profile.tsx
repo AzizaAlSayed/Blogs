1ad31dc1ae2851c1a77c6437e9d617bf3b80fdf4
laraSaidSayedAhmad <azizasayedahmad@gmail.com>
2022-01-02 13:03:45 +0200

    finish profile

--- src/components/ProfilePage/profile.tsx
+++ src/components/ProfilePage/profile.tsx
@@ -1,7 +1,3 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Article, { ArticleProps } from "../HomePage/Article";
 import ArticleList, { ArticlesResponse } from "../HomePage/ArtilceList";
 
 const Profile: React.FC<{}> = () => {
@@ -18,7 +14,7 @@ const Profile: React.FC<{}> = () => {
                   src="https://api.realworld.io/images/demo-avatar.png"
                 />
                 <h4 ng-bind="::$ctrl.profile.username" className="ng-binding">
-                  {params.username}
+              
                 </h4>
                 <button
                   className="btn btn-sm action-btn ng-binding btn-outline-secondary"
@@ -28,7 +24,7 @@ const Profile: React.FC<{}> = () => {
                   ng-click="$ctrl.submit()"
                 >
                   <i className="ion-plus-round"></i>
-                  &nbsp; Follow {params.username}
+                  &nbsp; Follow
                 </button>
               </div>
             </div>
@@ -41,12 +37,12 @@ const Profile: React.FC<{}> = () => {
               <div className="articles-toggle">
                 <ul className="nav nav-pills outline-active">
                   <li className="nav-item">
-                    <a className="nav-link active" href="">
+                    <a className="nav-link active">
                       My Articles
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="">
+                    <a className="nav-link" >
                       Favorited Articles
                     </a>
                   </li>
